6dd9e417a913907d2a4bdfbd11c80c4dbaf3b1ac
gitaafu <aafreenwork521@gmail.com>
2024-04-01 17:51:59 +0530

    multiple images displayed

--- Buyer's main Page/buyer-main/src/app/components/gadgets/gadgets.component.ts
+++ Buyer's main Page/buyer-main/src/app/components/gadgets/gadgets.component.ts
@@ -13,40 +13,21 @@ import { ImageProcessingService } from 'src/app/service/image-processing.service
 export class GadgetsComponent {
 
   
-  ngOnInit():void{
-    this.displayProducts();
-   }
-    constructor(private productService:GetallproductsService, private imageProcessingService:ImageProcessingService){ }
-    data!:Product[];
-  
-    displayProducts(){
-      this.productService.getAllGadgets()
-      .pipe(
-        map((products: Product[]) => {
-          return products.map((product: Product) => {
-            // Transform the product using both functions
-            const productWithImages = this.imageProcessingService.createImages(product);
-           // const productWithThumbnail = this.imageProcessingService.createImage(productWithImages);
-            return productWithImages;
-          });
-        })
-      )
-      .subscribe(
-        (response:Product[])=>
-        {
-          console.log(response);
-          this.data=response;
-          
-        },
-        (error:HttpErrorResponse)=>{
-          console.error('Error Displaying Product', error);
-          if (error.status === 400) {
-            console.error('Details are not entered!!');
-            alert('Please make sure that you Enter all the Details');
-          } 
-          else{
-            alert('Error Adding product: ' + error.message);
-          }
-        }
-      );}
+    
+ ngOnInit():void{
+  this.displayProducts();
+ }
+  constructor(private productService:GetallproductsService){ }
+  data!:Product[];
+
+  displayProducts(){
+    this.productService.getAllGadgets().subscribe(res=>{
+      this.data=res;
+      console.log(res);
+    },
+    (error)=>{
+      console.log(error);
+    }
+    );
+  }
 }
--- Buyer's main Page/buyer-main/src/app/product-detail2/product-detail2.component.ts
+++ Buyer's main Page/buyer-main/src/app/product-detail2/product-detail2.component.ts
@@ -3,6 +3,8 @@ import { Product } from '../model/product.model';
 import { GetallproductsService } from '../service/getallproducts.service';
 import { ActivatedRoute } from '@angular/router';
 import { ImageProcessingService } from '../service/image-processing.service';
+import { FileHandle } from '../model/file-handle.model';
+import { SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-product-detail2',
@@ -11,13 +13,16 @@ import { ImageProcessingService } from '../service/image-processing.service';
 })
 export class ProductDetail2Component implements OnInit {
   product!:Product;
+
+
+
   constructor(private service:GetallproductsService, private active:ActivatedRoute, private imageProcessingService:ImageProcessingService){}
   ngOnInit(): void {
     let productid=this.active.snapshot.paramMap.get('product_id');
     //console.log("product id is",productid);
     productid && this.service.getProductById(productid).subscribe(
       (res)=>{
-      this.product=res;
+        this.product = this.imageProcessingService.createImages(res);
       console.log(res);
       },
       (error)=>
@@ -28,4 +33,5 @@ export class ProductDetail2Component implements OnInit {
     )
   }
 
+ 
 }
